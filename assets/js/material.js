8d7c67c618949b5e91f7a34d6f5071dd33a65624
JmyCloud <mujmy.main@gmail.com>
2025-04-06 04:22:57 +0200

    Auto commit

--- assets/js/material.js
+++ assets/js/material.js
@@ -27,7 +27,7 @@ el.classList.remove('tooltipped');
 }
 }
 } else {
-document.addEventListener("mouseenter", function (event) { 
+document.addEventListener("pointerenter", function (event) { 
 if (!(event.target instanceof Element)) return;
 const btn = event.target.closest(".tooltipped");
 if (!btn) return;
